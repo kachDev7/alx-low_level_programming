06af335fe0eea1aeb9ad39d5cd482398e7b41bdf
kachDev7 <kachiamogu47@gmail.com>
2022-03-11 14:47:32 -0800

    my commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,6 @@
 
 /**
  * main - Entry point
- *  
  *  Return: Always 0 (Success)
  */
 int main(void)
@@ -12,5 +11,5 @@ int main(void)
 	  printf("Size of a long int: %d bytes(s)\n", sizeof(long int));
 	 printf("size of a long long int: %d bytes(s)\n", sizeof(long long int));
 	printf("Size of a float: %d bytes(s)\n", sizeof(float));
-	return (0);	
+	return(0);
 }
