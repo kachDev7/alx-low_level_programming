1f56dd84771e920b8185d714bdbec5df53d5de35
kachDev7 <kachiamogu47@gmail.com>
2022-03-15 15:51:13 -0700

    my commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *  *add - sum of two numbers.
+ *   *@n: number to be added.
+ *    *@p: number to be added.
+ *     *
+ *      *Return: sum.
+ *       */
+int add(int n, int p)
+{
+	int sum = n + p;
+	return (sum);
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+my Readme file for functions and nested loops task
